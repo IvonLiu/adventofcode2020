0e189c83ffa995498597cc9ca4c4dff2483e454d
Ivon Liu <me@ivonliu.com>
2020-12-22 19:12:24 -0800

    Finish day 3 part 2

--- day3/solution.cpp
+++ day3/solution.cpp
@@ -4,29 +4,42 @@
 #include <vector>
 using namespace std;
 
-int main() {
-	vector<string> trees;
+int checkTree(int dx, int dy) {
+	int x = 0;
+	int y = 0;
+	int count = 0;
+
 	string line;
 	ifstream input ("input.txt");
 	if (input.is_open()) {
 		while (getline(input, line)) {
-			trees.push_back(line);
+			if (y % dy == 0) {
+				char c = line[x];
+				if (c == '#') {
+					count++;
+				}
+				x = (x + dx) % line.length();
+			}
+			y++;
 		}
 		input.close();
 	}
 
-	int x = 0;
-	int n = trees[0].length();
-	int count = 0;
+	return count;
+}
 
-	for (auto it = trees.begin(); it != trees.end(); it++) {
-		char c = (*it)[x];
-		if (c == '#') {
-			count++;
-		}
-		x = (x + 3) % n;
+int main() {
+
+	int slopes[5][2] = {{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}};
+	long product = 1;
+
+	for (auto &slope : slopes) {
+		int count = checkTree(slope[0], slope[1]);
+		cout << slope[0] << ", " << slope[1] << " => " << count << endl;
+		product *= count;
 	}
 
-	cout << count << endl;
+	cout << product << endl;
+
 	return 0;
 }
