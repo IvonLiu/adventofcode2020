5d6f1f5ef4be2382c95a2ebe52e01f5a36c69daf
Ivon Liu <me@ivonliu.com>
2020-12-25 23:21:57 -0800

    Day 8 part 2

--- day8/solution.cpp
+++ day8/solution.cpp
@@ -3,43 +3,76 @@
 #include <string>
 #include <tuple>
 #include <vector>
+#include <unordered_set>
 using namespace std;
 
+bool testLoop(vector<tuple<string, int>> &instructions, int &accumulator) {
+    accumulator = 0;
+    auto inst = instructions.begin();
+    unordered_set<int> visited;
+
+    cout << "Testing loop " << inst - instructions.begin() << endl;
+
+    while (visited.find(inst - instructions.begin()) == visited.end() && inst < instructions.end()) {
+        string cmd = get<0>(*inst);
+        int val = get<1>(*inst);
+        visited.insert(inst - instructions.begin());
+
+        if (cmd == "acc") {
+            accumulator += val;
+            inst++;
+        } else if (cmd == "jmp") {
+            inst += val;
+        } else {
+            inst++;
+        }
+
+        cout << cmd << " " << val << " | " << accumulator << " | " << inst - instructions.begin() << endl;
+    }
+
+    return inst >= instructions.end();
+}
+
+int fixLoop(vector<tuple<string, int>> &instructions) {
+    int accumulator;
+    for (auto &inst : instructions) {
+        if (get<0>(inst) == "jmp") {
+            get<0>(inst) = "nop";
+            if (testLoop(instructions, accumulator)) {
+                return accumulator;
+            }
+            get<0>(inst) = "jmp";
+        } else if (get<0>(inst) == "nop") {
+            get<0>(inst) = "jmp";
+            if (testLoop(instructions, accumulator)) {
+                return accumulator;
+            }
+            get<0>(inst) = "nop";
+        }
+    }
+    return -1;
+}
+
 int main() {
-	vector<tuple<string, int, bool>> instructions;
+    vector<tuple<string, int>> instructions;
 
-	string line;
-	ifstream input ("input.txt");
+    string line;
+    ifstream input ("input.txt");
 
-	if (input.is_open()) {
-		while (getline(input, line)) {
-			int idx = line.find(" ");
-			string cmd = line.substr(0, idx);
-			int val = stoi(line.substr(idx+1));
-			instructions.push_back(make_tuple(cmd, val, false));
-		}
-		input.close();
-	}
+    if (input.is_open()) {
+        while (getline(input, line)) {
+            int idx = line.find(" ");
+            string cmd = line.substr(0, idx);
+            int val = stoi(line.substr(idx+1));
+            instructions.push_back(make_tuple(cmd, val));
+        }
+        input.close();
+    }
 
-	int accumulator = 0;
-	auto inst = instructions.begin();
+    //int accumulator;
+    //bool noLoop = testLoop(instructions, accumulator);
 
-	while (!get<2>(*inst)) {
-		string cmd = get<0>(*inst);
-		int val = get<1>(*inst);
-		get<2>(*inst) = true;
+    int accumulator = fixLoop(instructions);
 
-		cout << cmd << " " << val << " | " << accumulator << endl;
-
-		if (cmd == "acc") {
-			accumulator += val;
-			inst++;
-		} else if (cmd == "jmp") {
-			inst += val;
-		} else {
-			inst++;
-		}
-	}
-
-	cout << accumulator << endl;
+    cout << accumulator << endl;
 }
