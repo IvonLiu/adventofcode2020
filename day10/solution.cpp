c6a99d0e79804ac5bab83488ff3138a418a4addb
Ivon Liu <me@ivonliu.com>
2020-12-26 22:11:43 -0800

    Day 10 part 2

--- day10/solution.cpp
+++ day10/solution.cpp
@@ -2,47 +2,60 @@
 #include <fstream>
 #include <string>
 #include <unordered_set>
+#include <vector>
 using namespace std;
 
 int main() {
 
-	string line;
-	ifstream input ("input.txt");
-	unordered_set<int> jolts;
-	int maxJolt = 0;
+    string line;
+    ifstream input ("input.txt");
+    unordered_set<int> jolts;
+    int maxJolt = 0;
 
-	if (input.is_open()) {
-		while (getline(input, line)) {
-			int val = stoi(line);
-			if (val > maxJolt) {
-				maxJolt = val;
-			}
-			jolts.insert(val);
-		}
-		input.close();
-	}
+    if (input.is_open()) {
+        while (getline(input, line)) {
+            int val = stoi(line);
+            if (val > maxJolt) {
+                maxJolt = val;
+            }
+            jolts.insert(val);
+        }
+        input.close();
+    }
 
-	int jolt = 0;
-	int count1 = 0;
-	int count3 = 1;
+    int jolt = 0;
+    int count1 = 0;
+    int count3 = 1;
 
-	while (jolt < maxJolt) {
-		bool found = false;
-		for (int i=1; i<=3; i++) {
-			if (jolts.find(jolt + i) != jolts.end()) {
-				found = true;
-				jolt = jolt + i;
-				if (i == 1) count1++;
-				if (i == 3) count3++;
-				break;
-			}
-		}
-		if (!found) {
-			break;
-		}
-	}
+    while (jolt < maxJolt) {
+        bool found = false;
+        for (int i=1; i<=3; i++) {
+            if (jolts.find(jolt + i) != jolts.end()) {
+                found = true;
+                jolt = jolt + i;
+                if (i == 1) count1++;
+                if (i == 3) count3++;
+                break;
+            }
+        }
+        if (!found) {
+            break;
+        }
+    }
 
-	cout << count1 << " * " << count3 << " = " << (count1 * count3) << endl;
+    cout << count1 << " * " << count3 << " = " << (count1 * count3) << endl;
 
-	return 0;
+    vector<long long> combinations (maxJolt+1, 0);
+    combinations[0] = 1;
+
+    for (int i = 0; i <= maxJolt; i++) {
+        if (jolts.find(i) != jolts.end()) {
+            combinations[i] += i - 3 >= 0 ? combinations[i-3] : 0;
+            combinations[i] += i - 2 >= 0 ? combinations[i-2] : 0;
+            combinations[i] += i - 1 >= 0 ? combinations[i-1] : 0;
+            cout << i << ": " << combinations[i] << endl;
+        }
+    }
+
+    return 0;
 }
