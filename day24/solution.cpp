fda26c2b5a2beb2e220c4961521405d308b537e8
Ivon Liu <me@ivonliu.com>
2021-01-06 19:04:54 -0800

    Day 24 part 2

--- day24/solution.cpp
+++ day24/solution.cpp
@@ -3,7 +3,62 @@
 #include <string>
 #include <sstream>
 #include <unordered_set>
+#include <unordered_map>
+#include <vector>
 using namespace std;
+
+void parse(const string &key, int &b1, int &b2) {
+	int pos = key.find("n");
+	b1 = stoi(key.substr(1, pos-1));
+	b2 = stoi(key.substr(pos+1));
+}
+
+vector<string>* neighbors(const string &key) {
+	vector<string>* ns = new vector<string>();
+	int b1, b2;
+	parse(key, b1, b2);
+	int directions[6][2] = {
+		{2, 0},
+		{1, -1},
+		{-1, -1},
+		{-2, 0},
+		{-1, 1},
+		{1, 1}
+	};
+	for (auto &dir : directions) {
+		int n1 = b1 + dir[0];
+		int n2 = b2 + dir[1];
+		ostringstream ss;
+		ss << "e" << n1 << "n" << n2;
+		ns->push_back(ss.str());
+	}
+
+	return ns;
+}
+
+void step(unordered_set<string> &flipped) {
+	unordered_map<string, int> neighborCounts;
+	for (const string &key : flipped) {
+		vector<string>* ns = neighbors(key);
+		for (string &n : *ns) {
+			if (neighborCounts.find(n) == neighborCounts.end()) {
+				neighborCounts[n] = 1;
+			} else {
+				neighborCounts[n]++;
+			}
+		}
+		delete ns;
+	}
+
+	unordered_set<string> newFlipped;
+	for (const auto &pair : neighborCounts) {
+		if (pair.second == 2 ||
+			(pair.second == 1 && flipped.find(pair.first) != flipped.end())) {
+			newFlipped.insert(pair.first);
+		}
+	}
+	flipped = newFlipped;
+}
 
 int main() {
 
@@ -36,7 +91,7 @@ int main() {
 			}
 
 			ostringstream ss;
-			ss << "b1" << b1 << "b2" << b2;
+			ss << "e" << b1 << "n" << b2;
 			string key = ss.str();
 
 			if (flipped.erase(key) == 0) {
@@ -47,6 +102,11 @@ int main() {
 	}
 
 	cout << flipped.size() << endl;
+
+	for (int i=1; i<=100; i++) {
+		step(flipped);
+		cout << "Day " << i << ": " << flipped.size() << endl;
+	}
 
 	return 0;
 }
